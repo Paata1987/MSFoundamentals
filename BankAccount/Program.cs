238d0102590c72ca5eba26fa679c44130132f352
unknown <paatatepnadze1@gmail.com>
2022-11-10 09:52:55 +0100

    create transaction class

--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -10,6 +10,10 @@ namespace BankAccount
     {
         static void Main(string[] args)
         {
+           
+
+            var account = new BankAccount("<name>", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
         }
     }
 }
